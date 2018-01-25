922d810b0ddff1680bf617723142a953c3e3746a
松江市まつえ産業支援センター <akashedwin646@gmail.com>
2018-01-25 11:11:46 +0900

    check

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,7 +15,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.<CSS class for field with error>'
   end
 
-  test "valid signup informatio" do
+  test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
